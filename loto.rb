d9708de5d1398734753986a9a32ff1e5bf00d6eb
Pierre Merlin <Pierre Merlin>
2017-01-10 10:26:17 +0100

    loto 1srt implementation

--- loto.rb
+++ loto.rb
@@ -1,3 +1,6 @@
-number = rand 45
+available_balls = (1..45).to_a
+# first random picking
+picked_balls=[]
+(0..4).each { |i| picked_balls.push available_balls.delete_at rand(45-i) }
 
-puts number 
+puts "Le tirage du jour est : #{picked_balls.sort}" 
