dc3968b8b4ecdc33d42b243db2952907223a993b
Pierre Merlin <Pierre Merlin>
2017-01-10 10:35:53 +0100

    refact loto

--- loto.rb
+++ loto.rb
@@ -1,6 +1,5 @@
 available_balls = (1..45).to_a
-# first random picking
-picked_balls=[]
-(0..4).each { |i| picked_balls.push available_balls.delete_at rand(45-i) }
+# shuffle balls and take 5
+picked_balls = available_balls.shuffle.take 5
 
 puts "Le tirage du jour est : #{picked_balls.sort}" 
