36637965146ca05e402c76c3de39cbbc4a03c758
Pierre Merlin <Pierre Merlin>
2017-01-10 14:57:26 +0100

    supercagnote du vendredi 13

--- loto.rb
+++ loto.rb
@@ -1,3 +1,13 @@
+# affichage du montant de la cagnote
+# entre 100 et 500.000 Euros
+# le vendredi 13, la cagnote est de 2 millions
+is_vendredi_13 = true
+if is_vendredi_13
+  cagnote = 2_000_000
+else
+  cagnote = 100.000
+end
+puts "Le montant de la cagnote du jour est de #{cagnote}"
 available_balls = (1..45).to_a
 # shuffle balls and take 5
 picked_balls = available_balls.shuffle.take 5
