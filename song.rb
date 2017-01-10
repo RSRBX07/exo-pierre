da9b7cce3e70be221a4f4bd7282d11cdcc0f9579
Pierre Merlin <Pierre Merlin>
2017-01-10 16:49:44 +0100

    add song file

---
+++ song.rb
@@ -0,0 +1,21 @@
+=begin
+
+Chant de marche
+=============
+
+Ma poule n'a plus qu'vingt-neuf poussins (bis)
+Elle en a eu trente, la moitié d' soixante.
+refrain
+Allongeons la jambe, la jambe
+Car la route est longue! (bis)
+Ma poule n'a plus qu'vingt-huit poussins (bis)
+Elle en a eu trente, la moitié d' soixante.
+[...]
+avant dernier couplet
+Ma poule n'a plus qu'un seul poussin (bis)
+Elle en a eu trente, la moitié d' soixante.
+[...]
+dernier couplet
+Ma poule n'a plus aucun poussin (bis)
+Elle en a eu trente, la moitié d' soixante.
+=end
