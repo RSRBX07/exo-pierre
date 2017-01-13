1a4b309a95ff807d63622e71d9ce2f6e9df0d320
Pierre Merlin <pierrem@apido.org>
2017-01-13 07:31:26 +0100

    spell_in_french until 9999999999
    FIX-ME :
    dix-zero
    dix-un
    dix-deux
    dix-trois
    dix-quatre
    dix-cinq
    dix-six

--- french_number.rb
+++ french_number.rb
@@ -1,6 +1,8 @@
 def spell_in_french num
   units_strings = ["zero", "un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf"]
   return units_strings[num] if num < 10
+  deci_strings = ["","dix", "vingt", "trente", "quarante", "cinquante", "soixante", "soixante-dix", "quatrevingt", "quatrevingt-dix"]
+  return deci_strings[num/10] + "-" + spell_in_french(num % 10) if (10..99).include? num 
 end
 
-(0..9).each { |i| puts spell_in_french i }
+(0..99).each { |i| puts spell_in_french i }
