4c4f36ebd8ffd31f4123744c84bdf722ad7682d8
Pierre Merlin <pierrem@apido.org>
2017-01-16 12:37:54 +0100

    fix rebase conflict

--- french_number.rb
+++ french_number.rb
@@ -1,6 +1,6 @@
 def spell_in_french num
   units_strings = ["zero", "un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf"]
-  units_string[num]
+  return units_strings[num] if num < 10
 end
 
 (0..9).each { |i| puts spell_in_french i }
