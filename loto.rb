d22fb345521b3a0aed168887379e1b95f3387b94
Pierre Merlin <pierrem@apido.org>
2017-01-16 12:31:40 +0100

    WIP Loto#has_winner

--- loto.rb
+++ loto.rb
@@ -1,19 +1,72 @@
-# demander une grille de jeu
+require 'date'
+class Loto
 
-# affichage du montant de la cagnote
-# entre 100 et 500.000 Euros
-# le vendredi 13, la cagnote est de 2 millions
-is_vendredi_13 = false
-if is_vendredi_13
-  cagnote = 2_000_000
-else
-  cagnote = 100_000
+  def self.get_grid
+    grid = []
+    5.times do
+      input = gets.to_i
+      grid << input
+    end
+    grid
+  end
+
+  def self.get_flash
+    (1..45).to_a.shuffle.take 5
+  end
+
+  def has_winner?
+    #comprer tous les bulletins valides avec la grille gagnante
+    sorted_draw = draw.sort
+    @saved_grids.each do |grid|
+      sorted_grid = grid.sort
+      sorted_grid == sorted_draw
+    end
+  end
+
+  # enregistre une grille
+  # pour le loto courant
+  def validate_grid grid
+    @saved_grids ||= []
+    @saved_grids.push grid
+  end 
+  # demander une grille de jeu
+
+  # affichage du montant de la cagnote
+  # entre 100 et 500.000 Euros
+  # le vendredi 13, la cagnote est de 2 millions
+  def vendredi_13?
+    Date.today.day == 13 and Date.today.friday?
+  end
+
+  
+
+  def prize
+    cagnote = if vendredi_13?
+      2_000_000
+    else
+      100_000
+    end
+    puts "Le montant de la cagnote du jour est de #{cagnote}"
+    cagnote
+  end
+
+  def draw
+    available_balls = (1..45).to_a
+    # shuffle balls and take 5
+    @picked_balls ||= available_balls.shuffle.take(5)
+
+    puts "Le tirage du jour est : #{@picked_balls.sort}" 
+    @picked_balls
+  end
+
+  def check_grid grid
+    # afficher si gagne ou perdu
+    if grid.sort ==
+       draw.sort
+      puts "You win !"
+    else
+      puts "You loose !"
+    end
+  end
+  
 end
-puts "Le montant de la cagnote du jour est de #{cagnote}"
-available_balls = (1..45).to_a
-# shuffle balls and take 5
-picked_balls = available_balls.shuffle.take 5
-
-puts "Le tirage du jour est : #{picked_balls.sort}" 
-
-# afficher si gagne ou perdu
--- test.rb
+++ test.rb
@@ -1,9 +1,28 @@
-require './dice.rb'
+# test de dice.rb
+# require './dice.rb'
 
-my_dice = Dice.new
+# my_dice = Dice.new
 
-puts "lancer de de normal"
-puts my_dice.roll
+# puts "lancer de de normal"
+# puts my_dice.roll
 
-puts "lancer de de pipe"
-puts my_dice.roll 4
+# puts "lancer de de pipe"
+# puts my_dice.roll 4
+
+# test de loto
+
+require './loto.rb'
+
+loto_du_samedi = Loto.new
+2.times {loto_du_samedi.validate_grid Loto.get_flash}
+
+#.each { |grid| loto_du_samedi.check_grid grid }
+
+loto_du_lundi = Loto.new
+loto_du_lundi.validate_grid Loto.get_flash
+#loto_du_lundi.check_grid(grid)
+if loto_du_lundi.has_winner?
+  puts "Someone win"
+else
+  puts "nobody win"
+end
