f1d8cd260abe74d8728d7f40b61c606cc5cd5363
Pierre Merlin <pierrem@apido.org>
2017-01-16 09:49:34 +0100

    Hello class

---
+++ dice.rb
@@ -0,0 +1,15 @@
+def roll(cheated_value = nil)
+  cheated_value
+end
+
+puts "lancer de de normal"
+puts roll
+
+puts "lancer de de pipe"
+puts roll 4
+
+puts "Made with love @ #{place} by #{author}"
+
+class Dice
+  
+end
