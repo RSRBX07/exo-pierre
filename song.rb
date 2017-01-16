035b0dade470650eca0fc9e7f870a584158da939
Pierre Merlin <pierrem@apido.org>
2017-01-16 09:50:12 +0100

    song

--- song.rb
+++ song.rb
@@ -19,3 +19,21 @@ dernier couplet
 Ma poule n'a plus aucun poussin (bis)
 Elle en a eu trente, la moitié d' soixante.
 =end
+
+ch1 = "Ma poul' n'a plus que "
+
+ch2 = " poussins"
+
+ch3 = "Elle en avait trente.\n"
+
+ch4 ="Allongeons la jambe, \n Allongeons la jambe, la jambe.\n Car la route est longue.\n"
+
+
+
+29.times do |i|
+
+    ch = ch1 + String(30-i) + ch2 +",\n" + ch1 + String(30-i) + ch2 +".\n" + ch3  + ch4 + ch4 + "\n\n"    
+
+    puts ch
+
+end
