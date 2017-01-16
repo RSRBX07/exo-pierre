ad2f853c0b8959213589779760e7b41756eadabf
Pierre Merlin <pierrem@apido.org>
2017-01-16 09:50:06 +0100

    change loto

--- loto.rb
+++ loto.rb
@@ -1,3 +1,5 @@
+# demander une grille de jeu
+
 # affichage du montant de la cagnote
 # entre 100 et 500.000 Euros
 # le vendredi 13, la cagnote est de 2 millions
@@ -5,7 +7,7 @@ is_vendredi_13 = false
 if is_vendredi_13
   cagnote = 2_000_000
 else
-  cagnote = 100.000
+  cagnote = 100_000
 end
 puts "Le montant de la cagnote du jour est de #{cagnote}"
 available_balls = (1..45).to_a
@@ -13,3 +15,5 @@ available_balls = (1..45).to_a
 picked_balls = available_balls.shuffle.take 5
 
 puts "Le tirage du jour est : #{picked_balls.sort}" 
+
+# afficher si gagne ou perdu
