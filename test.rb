714d1ee8f7aacffc37ef21d8ce72d383d15da856
Pierre Merlin <pierrem@apido.org>
2017-01-17 10:33:54 +0100

    testing logo

--- test.rb
+++ test.rb
@@ -14,15 +14,17 @@
 require './loto.rb'
 
 loto_du_samedi = Loto.new
-2.times {loto_du_samedi.validate_grid Loto.get_flash}
+# 2.times {loto_du_samedi.validate_grid Loto.get_flash}
 
-#.each { |grid| loto_du_samedi.check_grid grid }
+# #.each { |grid| loto_du_samedi.check_grid grid }
 
-loto_du_lundi = Loto.new
-loto_du_lundi.validate_grid Loto.get_flash
-#loto_du_lundi.check_grid(grid)
-if loto_du_lundi.has_winner?
-  puts "Someone win"
-else
-  puts "nobody win"
-end
+# loto_du_lundi = Loto.new
+# loto_du_lundi.validate_grid Loto.get_flash
+# #loto_du_lundi.check_grid(grid)
+# if loto_du_lundi.has_winner?
+#   puts "Someone win"
+# else
+#   puts "nobody win"
+# end
+
+loto_du_samedi.prize
