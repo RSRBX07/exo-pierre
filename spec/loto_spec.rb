d88aa3a6ae12c899b31243848e3647cd786facd7
Pierre Merlin <pierrem@apido.org>
2017-01-17 15:19:33 +0100

    mv loto spec

--- spec/loto_spec.rb
+++ spec/loto_spec.rb
@@ -1,16 +1,3 @@
-# test de dice.rb
-# require './dice.rb'
-
-# my_dice = Dice.new
-
-# puts "lancer de de normal"
-# puts my_dice.roll
-
-# puts "lancer de de pipe"
-# puts my_dice.roll 4
-
-# test de loto
-
 require './loto.rb'
 
 loto_du_samedi = Loto.new
