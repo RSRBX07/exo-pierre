39f2eb59df454a682cda1b0567ae028a28d18c4a
Pierre Merlin <pierrem@apido.org>
2017-01-18 17:52:47 +0100

    WIP Vehicle counter

--- vehicle/vehicle.rb
+++ vehicle/vehicle.rb
@@ -1,6 +1,19 @@
+class Counter
+end
 class Vehicle
   attr_reader :position
   attr_reader :out_of_order
+
+  # return the number of existing objects (in mermory)
+  # open irb
+  # > Vehicle.count
+  # # => 0
+  # > Vehicle.new
+  # > Vehicle.count
+  # 1
+  def self.count
+
+  end
 
   def initialize
     puts "Im starting initialization my instance"
