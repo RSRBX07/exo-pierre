42e4f38990c711281ca2f604c728349a6f4888d5
Pierre Merlin <pierrem@apido.org>
2017-01-18 10:08:23 +0100

    RED : cannot play after draw

--- spec/loto_spec.rb
+++ spec/loto_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Loto do
 
   it 'give a radom flash grid' do
     expect(Loto.get_flash).not_to be_nil
-    expect(Loto.get_flash).to be_a Array
+    expect(Loto.get_flash).to be_an Array
     expect(Loto.get_flash.size).to equal 5
     expect(Loto.get_flash).not_to eql Loto.get_flash
   end
@@ -17,15 +17,27 @@ RSpec.describe Loto do
     loto_draw = loto.draw
     expect(loto).to respond_to :draw
     expect(loto_draw).not_to be_nil
-    expect(loto_draw).to be_a Array
+    expect(loto_draw).to be_an Array
     expect(loto_draw.size).to equal 5
+  end
+
+  it 'make a single draw for each instance' do
+    loto = Loto.new
+    loto_draw = loto.draw
     expect(loto_draw).to eql loto.draw
   end
+
   it 'validate a grid register this grid' do
      loto = Loto.new
      grid = [1,2,3,4,5]
      validated_grids = loto.validate_grid grid
      expect(validated_grids).to include grid
   end
-  it 'reject grid validation after draw'
+  it 'reject grid validation after draw' do
+    loto = Loto.new
+    loto.draw 
+    grid = [1,2,3,4,5]
+    validated_grids = loto.validate_grid grid
+    expect(validated_grids).not_to include grid 
+  end
 end
