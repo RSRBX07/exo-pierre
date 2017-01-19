36639e23462571c7710481858112c3aecc99c086
Pierre Merlin <pierrem@apido.org>
2017-01-19 12:31:34 +0100

    Hello File

--- vehicle/vehicle.rb
+++ vehicle/vehicle.rb
@@ -1,3 +1,16 @@
+# require 'file'
+
+class Counter
+  def add_one
+
+  end
+
+  def value
+    File.open "./tmp/counter.txt", "r" do |counter_file|
+      counter_file.each_line {|line| line }
+    end
+  end
+end
 class Vehicle
   attr_reader :position
   attr_reader :out_of_order
@@ -6,6 +19,11 @@ class Vehicle
     puts "Im starting initialization my instance"
     @out_of_order = false
     puts "Im finishing initialization my instance"
+    Counter.new.add_one
+  end
+
+  def self.count
+    Counter.new.value
   end
 
 
