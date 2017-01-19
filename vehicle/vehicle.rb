030e2d6c401b622a9bdec069d19e6a22b8a8f3d2
Pierre Merlin <pierrem@apido.org>
2017-01-19 13:48:04 +0100

    learn to read and write files

--- vehicle/vehicle.rb
+++ vehicle/vehicle.rb
@@ -2,12 +2,15 @@
 
 class Counter
   def add_one
-
+    new_val = value + 1 
+    File.open "./tmp/counter.txt", "w" do |counter_file|
+      counter_file.write new_val
+    end
   end
 
   def value
     File.open "./tmp/counter.txt", "r" do |counter_file|
-      counter_file.each_line {|line| line }
+      counter_file.each_line {|line| return line.to_i }
     end
   end
 end
