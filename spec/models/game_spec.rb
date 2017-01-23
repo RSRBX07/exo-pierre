d2928b7ba6903a37421a4ef12bdf1af3ddfcbf92
Pierre Merlin <pierrem@apido.org>
2017-01-23 18:25:42 +0100

    RED : Game.players should be initialized to 0.

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Game, type: :model, focus: true do
+  describe '#players' do
+    it 'has players' do
+      expect(subject).to respond_to :players
+    end
+    it 'players are initialized to 0' do
+      expect(subject.players).to equal(0)
+    end
+  end
 end
