58c524677ea6ef9a1996210bd2f75e4447e8e0e1
Pierre Merlin <pierrem@apido.org>
2017-01-23 18:30:04 +0100

    Green : Game's players are set to 0 by default

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170123154111) do
+ActiveRecord::Schema.define(version: 20170123172615) do
 
   create_table "games", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.integer  "players"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.integer  "players",     default: 0
   end
 
 end
