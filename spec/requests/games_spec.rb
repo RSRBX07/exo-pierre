c120461fd8ff71daa7269f7dc0c8c3d002cd6be8
Pierre Merlin <pierrem@apido.org>
2017-01-23 21:17:06 +0100

    red : GamesController#create persist data

--- spec/requests/games_spec.rb
+++ spec/requests/games_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe "Games", type: :request do
       expect(response).to have_http_status(200)
     end
   end
+  describe 'POST /games' do
+    it 'save a new game and persist attributes' do
+      game_params = {name: "FizzBuzz", description: "Dummy counting game", players: 4}
+      expect {post games_path, game: game_params}.to change{Game.count}.by 1
+      expect( game_params.stringify_keys.to_a - Game.last.attributes.stringify_keys.to_a).to be_empty
+    end
+  end
 end
