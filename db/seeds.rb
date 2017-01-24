098a76725a3fbfa688d8b75f2e7c779381ea4964
Pierre Merlin <pierrem@apido.org>
2017-01-24 11:28:46 +0100

    Add seeds with lotos

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Game.destroy_all
+Game.create [
+  {name: "Loto du lundi", description: "Le tirage est 12, 42, 11, 6  et 33", players: 2245985},
+  {name: "Loto du mardi", description: "Le tirage est 8, 42, 11, 6  et 33", players: 2885485},
+  {name: "Loto du mercredi", description: "Le tirage est 5, 42, 11, 6  et 33", players: 2115985},
+  {name: "Loto du jeudi", description: "Le tirage est 12, 2, 11, 26  et 33", players: 2775985},
+  {name: "Loto du vendredi", description: "Le tirage est 2, 42, 11, 6  et 33", players: 1245955}
+]
