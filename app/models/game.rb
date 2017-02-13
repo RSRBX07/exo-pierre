d6e63c394202e13df272aa79c34d59a522a17f3c
Pierre Merlin <pierrem@apido.org>
2017-02-13 12:29:19 +0100

    last changes

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,9 +1,20 @@
 class Game < ApplicationRecord
   validates :players, numericality: {greater_than_or_equal_to: 0}
   has_one :orchard
-  has_many :fruits through: :orchard
+  has_many :fruits, through: :orchard
 
+  default_scope { self.ordered_by_last_recently_played }
 
+  # return Game::ActiveRecord_Relation
+  def self.ordered_by_last_recently_played
+    self.order(updated_at: :desc)
+  end
+
+  def self.recent
+    start_range = 1.week.ago
+    stop_range = Time.now
+    self.where created_at: [start_range..stop_range]
+  end
 
   def add_player
     @players += 1
